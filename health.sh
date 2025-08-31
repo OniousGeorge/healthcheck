a5f15a80f1c2f8e01c95d475735ad5a48e72a37b
oniousg <oniousg20@gmail.com>
2025-08-31 16:43:37 -0400

    added user-level cpu usage and revised idle cpu scripts

--- health.sh
+++ health.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-idle=99.94
+idle=2
 echo 'current percentage of idle cpu usage: 99.94 percent' >> health.logs
+usr=0.01
+echo 'current percentage of user-level cpu usage: 0.01 percent' >> health.logs
