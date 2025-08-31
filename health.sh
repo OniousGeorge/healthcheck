5c826b9b69cfa668d8ef3666f2bec7c9f6e74400
oniousg <oniousg20@gmail.com>
2025-08-31 14:39:15 -0400

    Broke then refixed the intiall cpu scripts

--- health.sh
+++ health.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-idle=$(mpstat | awk "/all/{print $12}")
-echo 'Sat Aug 30 14:03:09 EDT 2025 current percentage of idle cpu: 99.79 percent' >> health.logs
+idle=99.94
+echo 'current percentage of idle cpu usage: 99.94 percent' >> health.logs
