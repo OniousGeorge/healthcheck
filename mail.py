846ccab373275e936b05ba4fcf472bbcea9b2cd4
oniousg <oniousg20@gmail.com>
2025-09-10 20:49:14 -0400

    Was able to have it send the health check to email but log is not readable

---
+++ mail.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+
+
+def get_env(*names):
+    for n in names:
+        v = os.getenv(n)
+        if v:
+            return v
+    return None
+    
+
+load_dotenv(dotenv_path="config.env")
+#email=os.getenv("email")
+email=get_env('email','Email','EMAIL')
+reciever_email=get_env('to_email','To_email','TO_EMAIL','Email_to')
+email_password=get_env('email_password','password','PASSWORD','EMAIL,PASSWORD','Email_password')
+subject=get_env('subject','Subject')
+body=get_env('body','BODY')
+provider=get_env('smtp_provider','SMTP_PROVIDER','Gmail')
+
+print(f"Email user:{email} \n password: {email_password} \n subject:{subject}\n body: {body}\nprovider: {provider}\n reciever_email: {reciever_email}")
+
+msg=MIMEMultipart()
+msg['From'] = email
+msg['To'] = reciever_email
+msg['Subject'] = subject
+
+msg.attach(MIMEText(body, 'plain'))
+
+with open('health.logs', 'rb') as f:
+    part=MIMEApplication(f.read(), Name='health.logs')
+    part['Content-Disposition'] = 'attachment; filename="health.logs"'
+    msg.attach(part)
+
+with smtplib.SMTP('smtp.gmail.com', 587) as server:
+    server.starttls()
+    server.login(email, email_password)
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
